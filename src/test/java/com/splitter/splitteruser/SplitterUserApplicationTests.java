d0e84bdd90abfdc4a1eeda87737a01e243f98847
ACER <sumit.jaiswal8888@gmail.com>
2023-12-20 14:00:38 +0530

    adding user config files

---
+++ src/test/java/com/splitter/splitteruser/SplitterUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitter.splitteruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitterUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
