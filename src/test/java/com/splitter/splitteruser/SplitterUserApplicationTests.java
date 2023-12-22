4f4468591bb4a817d5497b4f3c0d06c11668a186
Sumit Jaiswal <88160905+sumit2803s@users.noreply.github.com>
2023-12-22 13:38:00 +0530

    Update SplitterUserApplicationTests.java

--- src/test/java/com/splitter/splitteruser/SplitterUserApplicationTests.java
+++ src/test/java/com/splitter/splitteruser/SplitterUserApplicationTests.java
@@ -1,13 +1,10 @@
 package com.splitter.splitteruser;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SplitterUserApplicationTests {
-
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
 	}
-
 }
