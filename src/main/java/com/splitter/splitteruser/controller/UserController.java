3917f51dea0d6a378ca8b8877aea4cebe56cb8b3
Sumit Jaiswal <88160905+sumit2803s@users.noreply.github.com>
2023-12-22 13:46:32 +0530

    Update UserController.java

--- src/main/java/com/splitter/splitteruser/controller/UserController.java
+++ src/main/java/com/splitter/splitteruser/controller/UserController.java
@@ -5,13 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+    
     @GetMapping("/register")
 
     public String getRegisterPage(){
+        
     return "register_page";
     }
     @GetMapping("/login")
     public String getLoginPage() {
+        
         return "login_page";
     }
 }
