d60fa51f36648bc7d0155ff2257047c5feb9e7a4
Sumit Jaiswal <88160905+sumit2803s@users.noreply.github.com>
2023-12-22 20:19:16 +0530

    Update UsersService.java

--- src/main/java/com/splitter/splitteruser/service/UsersService.java
+++ src/main/java/com/splitter/splitteruser/service/UsersService.java
@@ -4,9 +4,7 @@ package com.splitter.splitteruser.service;
 import com.splitter.splitteruser.model.UserModel;
 import com.splitter.splitteruser.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
 @Service
-
 public class UsersService {
 
     private final UserRepository userRepository;
@@ -14,23 +12,17 @@ public class UsersService {
     public UsersService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
-    public UserModel registerUser(String login, String password, String email) {
+ public UserModel registerUser(String login, String password, String email) {
         if(login!=null && password!=null ) {
             return null;
 
         } else {
-
             UserModel userModel = new UserModel();
             userModel.setLogin(login);
             userModel.setPassword(password);
             userModel.setEmail(email);
             return userRepository.save(userModel);
-
-
-
-
-        }
+         }
     }
     public UserModel authenticate(String login,String password){
         return userRepository.findByLoginAndPassword(login,password).orElse(null);
